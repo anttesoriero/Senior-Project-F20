dc50c60442f8cff678257c30177a8d0f170beb37
DanAndresSanchez <sanchez.andres.dan@gmail.com>
2020-09-11 10:58:56 -0400

    Added the basical react-app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, Container } from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <h1>This is the website so far</h1>
+        <Button>This is a button</Button>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
