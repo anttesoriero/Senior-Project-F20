ec3d51b998efbf4d9e6a677c85b38dc8f767e468
Anthony Tesoriero <anttesoriero@gmail.com>
2020-09-28 19:23:18 -0400

    Updated navbar to match design doc. Still needs styling work

--- frontend/src/Components/Navigation.tsx
+++ frontend/src/Components/Navigation.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
-import { Button, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
+import { Button, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown,
+    DropdownToggle, DropdownMenu, DropdownItem, NavbarText, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
 
 const Navigation = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -16,10 +17,16 @@ const Navigation = () => {
             <Collapse isOpen={isOpen} navbar>
                 <Nav navbar className="mr-auto">
                     <NavItem>
-                        <NavLink href="/taskboard">Task Board</NavLink>
+                        <NavLink href="/taskboard">Job Board</NavLink>
+                    </NavItem>
+                    {/*<NavItem>
+                        <NavLink href="/profile">Profile</NavLink>
+                    </NavItem>*/}
+                    <NavItem>
+                        <NavLink href="/findjobs">Find a Job</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink href="/profile">Profile</NavLink>
+                        <NavLink href="/listjobs">List a Job</NavLink>
                     </NavItem>
                 </Nav>
                 {/* <Button color="light" id="Popover1" type="button">Profile</Button> */}
@@ -36,6 +43,25 @@ const Navigation = () => {
                         <br />
                     </div>
                 </Popover>               */}
+
+                {/* Right side of nav bar */}
+                <UncontrolledDropdown navbar inNavbar>
+                    <DropdownToggle nav caret>
+                        Profile
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                        <DropdownItem href="/profile">
+                            Profile Home
+                        </DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem href="/upcomingjobs">
+                            Upcoming Jobs
+                        </DropdownItem>
+                        <DropdownItem href="/offers">
+                            Offers
+                        </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
             </Collapse>
         </Navbar>
     );
