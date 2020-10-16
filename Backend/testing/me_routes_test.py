d1d8e9d2a7110e79330ebd55ceed4e6b4c2445f0
Nick Romeo <romeon7@students.rowan.edu>
2020-10-16 19:24:24 -0400

    Everything tests correctly except for editInformation (See notes on line 87)

--- Backend/testing/me_routes_test.py
+++ Backend/testing/me_routes_test.py
@@ -76,3 +76,40 @@ def test_getPostedTasks():
     assert response.status_code == 200, "test_getPostedTasks()"
 
 test_getPostedTasks()
+
+# Tests editInformation for success
+def test_editInformation():
+    endpoint = "editInformation"
+
+    inputData = {
+        "email": "me_routes_test@gmail.com",
+        "firstName": "Me",
+        "lastName": "Test",                     # me\me_routes.py", line 109
+                                                # Everything else sets correctly when last name isnt included
+        "preferredName": "Nick",
+        "phoneNumber": "8562564000"
+    }
+    inputData = json.dumps(inputData)
+
+    response = requests.put(
+        base_me + endpoint,
+        headers=headers,
+        data=inputData
+    )
+    
+    assert response.status_code == 200, "test_editInformation()" # Returns 500 error
+
+test_editInformation()
+
+# Tests deleteAccount for success
+def test_deleteAccount():
+    endpoint = "deleteAccount"
+
+    response = requests.delete(
+        base_me + endpoint,
+        headers=headers
+    )
+    
+    assert response.status_code == 200, "test_deleteAccount()"
+
+test_deleteAccount()
