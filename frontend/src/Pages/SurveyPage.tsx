e9905f7a6fdf933c82b1cf598d312d9b8d94529f
Anthony <antcatalina1@gmail.com>
2020-10-18 17:50:57 -0400

    Survey page fixes, trying to get survey from backend

--- frontend/src/Pages/SurveyPage.tsx
+++ frontend/src/Pages/SurveyPage.tsx
@@ -1,103 +1,135 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navigation from '../Components/Navigation';
 import Footer from '../Components/Footer';
 import {Container, Row, Col, Button, Media, Badge} from 'reactstrap';
+import axios from 'axios';
+
+type surveyState = {
+  active: boolean;
+  answerA: string;
+  answerB: string;
+  answerC: string;
+  answerD: string;
+  answerE: string;
+  question: string;
+  surveyId: number
+}
 
 const SurveyPage = () => {
-  return (
-  <div>
-    <Navigation/>
-    <Container>
-       <h1 id="centered" style={{ fontWeight: 'bold' }}>Survey Page</h1>
-       <br/>
+  const token = localStorage.getItem('access_token');
+  const [survey, setSurvey]  = useState<surveyState>();
 
-       <h2 id="centered">Here's your daily survey</h2>
-       <p id="centered">This survey helps us make your experience better!</p>
-       <hr/>
+  const getSurvey = async () => {
+    {/* Example of sending authorized request. Get can take mulyiple parameters, in this case 2.
+        First one is the endpoint and second is the authorization headers */}
+    await axios.get('http://127.0.0.1:5000/survey/getSurvey', 
+    { headers: { Authorization: `Bearer ${token}` } })
+    .then( response => {
+        console.log(response);
+        setSurvey(response.data)
+    })
+    .catch( error => {
+        console.log(error);
+    });   
+}
 
-       <h4 id="centered" style={{ fontWeight: 'bold' }} >[Example Question ... ?]</h4>
-       <p id="centered">Some more information if needed</p>
+  useEffect(()=> {
+    getSurvey();
+}, []);
 
-       <div className="container">
-          <div className="row mt-5">
-            <div className="col-sm-12">
+    return (
+     <div>
+      <Navigation/>
+      <Container>
+         <h1 id="centered" style={{ fontWeight: 'bold' }}>Survey Page</h1>
+        <br/>
 
-              <form>
+        <h2 id="centered">Here's your daily survey</h2>
+        <p id="centered">This survey helps us make your experience better!</p>
+        <hr/>
+      <Row>
+        {survey ?
+           <h4 id="centered" style={{ fontWeight: 'bold' }} >Example Question : {survey.question}</h4>
+           :
+           <p id="centered">Some more information if needed</p>
+         }
+      </Row>
 
-                <div className="form-check">
-                  <label>
-                    <input
-                    type="radio"
-                    name="choice"
-                    value="option1"
-                    checked={true}
-                    className="form-check-input"
-                  />
-                  Example Choice #1
-                  </label>
-                </div>
+          <div className="container">
+            <div className="row mt-5">
+              <div className="col-sm-12">
+                  
+                   <form>
+                    <div className="form-check">
+                     <label>
+                     <input
+                     type="radio"
+                     name="choice"
+                     value="option1"
+                     checked={true}
+                     className="form-check-input"
+                     />
+                     </label>
+                   </div>
 
-                <div className="form-check">
-                  <label>
-                    <input
-                    type="radio"
-                    name="choice"
-                    value="option2"
-                    checked={true}
-                    className="form-check-input"
-                  />
-                  Example Choice #2
-                  </label>
-                </div>
+                    <div className="form-check">
+                      <label>
+                      <input
+                      type="radio"
+                      name="choice"
+                      value="option1"
+                      checked={true}
+                      className="form-check-input"
+                     />
+                      </label>
+                    </div>
 
-                <div className="form-check">
-                  <label>
-                    <input
-                    type="radio"
-                    name="choice"
-                    value="option3"
-                    checked={true}
-                    className="form-check-input"
-                  />
-                  Example Choice #3
-                  </label>
-                </div>
+                  <div className="form-check">
+                      <label>
+                      <input
+                      type="radio"
+                      name="choice"
+                      value="option1"
+                      checked={true}
+                      className="form-check-input"
+                     />
+                      </label>
+                    </div>
 
-                <div className="form-check">
-                  <label>
-                    <input
-                    type="radio"
-                    name="choice"
-                    value="option4"
-                    checked={true}
-                    className="form-check-input"
-                  />
-                  Example Choice #4
-                  </label>
-                </div>
+                  <div className="form-check">
+                      <label>
+                      <input
+                      type="radio"
+                      name="choice"
+                      value="option1"
+                      checked={true}
+                      className="form-check-input"
+                     />
+                      </label>
+                    </div>
 
-                <div className="form-check">
-                  <label>
-                    <input
-                    type="radio"
-                    name="choice"
-                    value="option5"
-                    checked={true}
-                    className="form-check-input"
-                  />
-                  Example Choice #5
-                  </label>
-                </div>
+                  <div className="form-check">
+                      <label>
+                      <input
+                      type="radio"
+                      name="choice"
+                      value="option1"
+                      checked={true}
+                      className="form-check-input"
+                     />
+                      </label>
+                    </div>
 
-                <Row>
+                  <Row>
                     <Col><hr/></Col>
-                    <Col id="centered"><Button href="/survey" color="secondary">Previous</Button></Col>
+                    <Col id="centered"><Button href="javascript:history.go(-1)" color="secondary">Previous</Button></Col>
                     <Col id="centered"><Button href="/survey" color="primary">Next</Button></Col>
                     <Col><hr/></Col>
-                </Row>
-                <h4> </h4>
+                  </Row>
+                  <h4> </h4>
 
-      </form>
+                   </form>
+                
 
     </div>
   </div>
