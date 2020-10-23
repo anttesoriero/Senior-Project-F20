a3ae0e96b45e6315d19d7501538bb2f650770e27
Anthony Tesoriero <anttesoriero@gmail.com>
2020-10-23 10:53:32 -0400

    Start of ListingPage

--- frontend/src/Pages/ListingPage.tsx
+++ frontend/src/Pages/ListingPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Navigation from '../Components/Navigation';
-import {Container, Row, Col, Button, Media, Badge} from 'reactstrap';
+import {Container, Row, Col, Button, Media, Badge, Form, FormGroup, Input, Label, CustomInput} from 'reactstrap';
 import Footer from "../Components/Footer";
 import PlaceholderImage from "../Styles/Images/placeholder.jpg"
 import axios from 'axios';
@@ -14,25 +14,6 @@ type userState = {
 
 const ListingPage = () => {
     const token = localStorage.getItem('access_token');
-    const [user, setUser]  = useState<userState>();
-
-    const getUser = async () => {
-        {/* Example of sending authorized request. Get can take mulyiple parameters, in this case 2.
-            First one is the endpoint and second is the authorization headers */}
-        await axios.get('http://127.0.0.1:5000/me/getProfile', 
-        { headers: { Authorization: `Bearer ${token}` } })
-        .then( response => {
-            console.log(response.data);
-            setUser(response.data)
-        })
-        .catch( error => {
-            console.log(error);
-        });   
-    }
-    
-    useEffect(()=> {
-        getUser();
-    }, []);
 
     return (
         <div>
@@ -40,6 +21,63 @@ const ListingPage = () => {
             <Container>
                 <h1 id="centered" style={{ fontWeight: 'bold' }}>List a new Task</h1>
                 <br/>
+
+                <div className="centered">
+                    <Form>
+                        {/* Row 1 - Name & Category */}
+                        <Row>
+                            <Col>
+                                <FormGroup>
+                                    <Label for="taskName"><h4>Task Name</h4></Label>
+                                    <Input type="text" name="taskName" id="taskName" placeholder="Task Name" required/>
+                                </FormGroup>
+                            </Col>
+                            <Col>
+                                <FormGroup>
+                                    <Label for="taskCategory"><h4>Task Category</h4></Label>
+                                    <Input type="select" name="taskCategory" id="taskCategory" required>
+                                        <option selected disabled>Select Category</option>
+                                        <option>Cat 1</option>
+                                        <option>Cat 2</option>
+                                        <option>Cat 3</option>
+                                        <option>Cat 4</option>
+                                    </Input>
+                                </FormGroup>
+                            </Col>
+                        </Row>
+
+                        {/* Row 2 - Description & Tools */}
+                        <Row>
+                            <Col>
+                                <FormGroup>
+                                    <Label for="taskDesc"><h4>Task Description</h4></Label>
+                                    <Input type="textarea" name="taskDesc" id="taskDesc" placeholder="Task Description" required/>
+                                </FormGroup>
+                            </Col>
+                            <Col>
+                                <FormGroup>
+                                    <Label for="taskTools"><h4>Required Tools</h4></Label>
+                                    <div>
+                                        <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Tool 1" />
+                                        <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Tool 2" />
+                                        <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Tool 3" />
+                                        <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Tool 4" />
+                                    </div>
+                                </FormGroup>
+                            </Col>
+                        </Row>
+
+                        {/* Pay Rate */}
+                        <div className="centered">
+                            <FormGroup>
+                                <Label for="taskPayRate"><h4>Pay Rate</h4></Label>
+                                <Input type="number" name="taskPayRate" id="taskPayRate" placeholder="$60" min="15" required/>
+                            </FormGroup>
+                        </div>
+                    </Form>
+                </div>
+
+                <hr />
 
                 
             </Container>
