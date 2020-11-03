709544a2ef22353f48f16f9306704a1ab5193c13
Anthony Tesoriero <anttesoriero@gmail.com>
2020-11-02 20:45:34 -0500

    Created scrollable task section

--- frontend/src/Components/Maps.tsx
+++ frontend/src/Components/Maps.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import axios from 'axios';
 import { TileLayer, Marker, Popup, MapContainer, CircleMarker, Tooltip, Circle } from 'react-leaflet';
 import { LatLngTuple } from 'leaflet';
+import PlaceholderImage from "../Styles/Images/placeholder.jpg"
+import { Container } from 'reactstrap';
 
 const Maps = (prop: {scrollBool: boolean}) => {
     const rowanLocation: LatLngTuple = [39.7089, -75.1183]
     const anthony: LatLngTuple = [39.7051596, -75.11357028778912]
     
     return (
-        <MapContainer center={rowanLocation} zoom={15} scrollWheelZoom={prop.scrollBool} style={{height: 550}}>
+        <MapContainer center={rowanLocation} zoom={15} scrollWheelZoom={prop.scrollBool} style={{height: 700}}>
             <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -27,7 +29,14 @@ const Maps = (prop: {scrollBool: boolean}) => {
             </Marker>
             <Marker position={[39.703, -75.113]}>
                 <Popup>
-                    <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
+                    <div style={{height: "350px"}}>
+                        <Container>
+                            <img src={PlaceholderImage} width="100%vw"/>
+                            <h1>Task Title</h1>
+                            <h2>Category</h2>
+                            <h3>$20 for 2 hours</h3>
+                        </Container>
+                    </div>
                 </Popup>
             </Marker>
 
@@ -42,6 +51,17 @@ const Maps = (prop: {scrollBool: boolean}) => {
                 pathOptions={{ fillColor: 'green' }}
                 radius={200}>
                 <Tooltip sticky>This is a Circle - It's overlapping, and has a sticky Tooltip</Tooltip>
+            </Circle>
+            <Circle
+                center={[39.7085, -75.110]}
+                pathOptions={{ fillColor: 'orange' }}
+                radius={100}>
+                <Popup>
+                    <img src={PlaceholderImage} width="100%vw"/>
+                    <h1>Task Title</h1>
+                    <h2>Category</h2>
+                    <h3>$20 for 2 hours</h3>
+                </Popup>
             </Circle>
             <CircleMarker
                 center={rowanLocation}
--- frontend/src/Pages/TestingPage.tsx
+++ frontend/src/Pages/TestingPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Navigation from '../Components/Navigation';
-import {Col, Container, Row} from 'reactstrap';
+import {Button, Card, CardBody, Col, Container, Row} from 'reactstrap';
 import Footer from "../Components/Footer";
 import axios from 'axios';
 import Maps from "../Components/Maps";
@@ -13,6 +13,8 @@ const TestingPage = () => {
     return (
         <div>
             <Navigation/>
+
+            {/*}
             <Container>
                 <h1>React-Leaflet Map (Maps Component)</h1>
                 <br />
@@ -26,7 +28,46 @@ const TestingPage = () => {
             
             <Maps scrollBool={false} />
             <br />
-                <TaskCard title='Testing Hover' offerer='USER' price='00' description='DESCRIPTION'/>
+
+            <Container><h5 >Two sided</h5></Container>
+            */}
+
+            {/* 
+                TODO: Make map markers popup onHover for Taskcards 
+                NOTE: Circle color could correspond to main super-categories
+            */}
+
+            <Row>
+                {/* Left - TaskCards */}
+                <Col xs="4">
+                    <Container>
+                        <div className="col-scroll">
+                            <h3 id="top" className="centered">Tasks</h3>
+                            <TaskCard title='Task 1' offerer='USER' price='00' description='DESCRIPTION'/>
+                            <TaskCard title='Task 2' offerer='USER' price='00' description='DESCRIPTION'/>
+                            <TaskCard title='Task 3' offerer='USER' price='00' description='DESCRIPTION'/>
+                            <TaskCard title='Task 4' offerer='USER' price='00' description='DESCRIPTION'/>
+                            <TaskCard title='Task 5' offerer='USER' price='00' description='DESCRIPTION'/>
+                            <TaskCard title='Task 6' offerer='USER' price='00' description='DESCRIPTION'/>
+                            <TaskCard title='Task 7' offerer='USER' price='00' description='DESCRIPTION'/>
+
+                            <Card>
+                                <CardBody>
+                                    <h4 className="centered">No More Tasks in this Area</h4>
+                                    <br />
+                                    <div><Button className={'task centered'} href="#top">Back to Top</Button></div>
+                                </CardBody>
+                            </Card>
+                        </div>
+                    </Container>
+                </Col>
+
+                {/* Right - Map */}
+                <Col xs="8">
+                    <Maps scrollBool={true} />
+                </Col>
+            </Row>
+                
             <Footer/>
         </div>
     );
