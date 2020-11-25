9d26f057d6126f687e61eddb05153927fb30c31a
Anthony Tesoriero <anttesoriero@gmail.com>
2020-11-25 14:31:20 -0500

    Refine Search Style setup - Need actual connection

--- frontend/src/Components/RefineSearch.tsx
+++ frontend/src/Components/RefineSearch.tsx
@@ -1,134 +1,51 @@
 import React, { useState } from 'react'
-import axios from 'axios';
-import PlaceholderImage from "../Styles/Images/placeholder.jpg"
 import { Button, Col, Container, Form, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader, Row, InputGroup, InputGroupAddon } from 'reactstrap';
-import CategoryDropdown from './CategoryDropdown';
+import { Field, Formik } from 'formik';
 
-const RefineSearch = (props) => {
-    {/* prop: {scrollBool: boolean} */}
-    const {
-        buttonLabel,
-        className
-      } = props;
-    const [modal, setModal] = useState(false);
-    const [backdrop, setBackdrop] = useState(true);
-    const [keyboard, setKeyboard] = useState(true);
-
-    const toggle = () => setModal(!modal);
-
-    const changeKeyboard = e => {
-        setKeyboard(e.currentTarget.checked);
-    }
-    
-    
+const RefineSearch = (props) => {    
     
     return (
-        <div style={{background: "#d6d6d6"}}>
-            <Container style={{height: "45px"}}>
-                <Form inline onSubmit={(e) => e.preventDefault()}>
-                    
+        <div className="centered" style={{background: "#d6d6d6", height: "45px"}}>
+            {/* <Formik initialValues={{ categoryId: 1, search: '', minPayRate: 0, maxPayRate: 0 }} onSubmit={data => }> */}
+            {/* Change "Input"'s to "Field"'s when Formik used */}
+                <Form inline>
+                    {/* Title Search Bar */}
                     <FormGroup>
-                        <Label for="searchBar"><h5><b>Search</b></h5></Label>{' '}
-                        <Input type="text" name="searchBar" id="searchBar" placeholder="Task Title" />
+                        <Label for="search"><h5><b>Search</b></h5></Label>{' '}
+                        <Input type="text" name="search" id="search" placeholder="Task Title" />
                     </FormGroup>
-                    <FormGroup className="mx-2" check>
-                        <Label check><Input type="checkbox" checked={keyboard} onChange={changeKeyboard} /></Label>
+
+                    {/* Select Task Category */}
+                    <FormGroup>
+                        <Label for="categoryId"><h5> | <b>Task Category</b></h5></Label>
+                        <Input type="select" name="categoryId" as={Input}>
+                            <option value="1" selected>Select Category</option>
+                            <option value="2">Yard Work</option>
+                            <option value="3">Transportation</option>
+                            <option value="4">Cleaning</option>
+                            <option value="5">Moving</option>
+                            <option value="6">Care-Taking</option>
+                            <option value="7">Cooking</option>
+                        </Input>
                     </FormGroup>
-                    {' '}
-                    <Button color="danger" onClick={toggle}>Refine Search</Button>
+
+                    {/* Select Duration */}
+                    <FormGroup>
+                        <Label for="duration"><h5> | <b>Duration </b> </h5></Label>
+                        <Input type="select" name="duration" id="duration">
+                            <option selected>Select Duration</option>
+                            <option value="30">{"< 30 Minutes"}</option>
+                            <option value="45">{"< 45 Minutes"}</option>
+                            <option value="60">{"< 60 Minutes"}</option>
+                            <option value="90">{"< 90 Minutes"}</option>
+                            <option value="120">{"< 120 Minutes"}</option>
+                        </Input>
+                    </FormGroup>
+
+                    {/* Submit Button */}
+                    <Button color="danger">Refine Search</Button>
                 </Form>
-
-                <Modal isOpen={modal} toggle={toggle} className={className} backdrop={backdrop} keyboard={keyboard} size="lg">
-                    <ModalHeader toggle={toggle}><h4>Refine Search</h4></ModalHeader>
-                    <ModalBody>
-                        
-
-                        <Row>
-                            <Col>
-                                <h4 className="centered">Pay Rate</h4>
-                            </Col>
-                            <Col>
-                                <h4 className="centered">Category</h4>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <InputGroup inline>
-                                    <Col xs="5">
-                                        {/* <InputGroupAddon addonType="prepend">$</InputGroupAddon> */}
-                                        <Input type="number" 
-                                            name="minPayRate" 
-                                            id="minPayRate" 
-                                            placeholder="Min" 
-                                            min="15" />
-                                    </Col>
-                                    <Col xs="1">-</Col>
-                                    <Col xs="5">
-                                        {/* <InputGroupAddon addonType="prepend">$</InputGroupAddon> */}
-                                        <Input type="number" 
-                                            name="maxPayRate" 
-                                            id="maxPayRate" 
-                                            placeholder="Max" 
-                                            min="15" />
-                                    </Col>
-                                </InputGroup>
-                            </Col>
-                            
-                            <Col>
-                            <FormGroup>
-                                    <Input type="select" name="taskCategory" id="taskCategory">
-                                            {/* "Select Category" is value=1 because "Yard Work" 
-                                                keeps displaying first even though it's second.
-                                                When calling the endpoint, the categoryId is substracted by one. */}
-                                            <option value="1" selected disabled>Select Category</option>
-                                            <option value="2">Yard Work</option>
-                                            <option value="3">Transportation</option>
-                                            <option value="4">Cleaning</option>
-                                            <option value="5">Moving</option>
-                                            <option value="6">Care-Taking</option>
-                                            <option value="7">Cooking</option>
-                                        {/* The Component below doesn't work */}
-                                        {/* <CategoryDropdown categoryList={['SAMPLE USAGE','Educational','Fitness','IMPORT OTHERS FROM FULL LIST']} /> */}
-                                    </Input>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-
-
-                        <Row>
-                            <Col>
-                                <h4 className="centered">--</h4>
-                            </Col>
-                            <Col>
-                                <h4 className="centered">Distance</h4>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                
-                            </Col>
-                            
-                            <Col>
-                            <FormGroup>
-                                    <Input type="select" name="distance" id="distance">
-                                        <option selected>Select Distance</option>
-                                        <option value="1">{"< 5 Miles"}</option>
-                                        <option value="2">{"< 10 Miles"}</option>
-                                        <option value="3">{"< 20 Miles"}</option>
-                                        <option value="4">{"< 50 Miles"}</option>
-                                        <option value="5">{"> 50 Miles"}</option>
-                                    </Input>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="primary" onClick={toggle}>Refine</Button>{' '}
-                        <Button color="secondary" onClick={toggle}>Cancel</Button>
-                    </ModalFooter>
-                </Modal>
-            </Container>
+            {/* </Formik> */}
         </div>
     );
   }
