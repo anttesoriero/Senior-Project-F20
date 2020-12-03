4583458dd9d432f087a6402e03e7b068b1fb9c6e
Steven Jiang <jiangs1@students.rowan.edu>
2020-12-03 00:00:08 -0500

    got rid of most oauth stuff

--- Backend/start.py
+++ Backend/start.py
@@ -6,7 +6,6 @@ from waitress import serve
 
 # Module imports
 from app import app
-import os
 
 # Is this a production environment?
 PRODUCTION = False
@@ -20,7 +19,4 @@ if __name__ == '__main__':
         serve(app, host='0.0.0.0', port=80)
     else:
         # Runs as dev server, should be on localhost 5000
-
-        # OAuth2 requires https, so for development, we can turn the check off
-        os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
         app.run()
